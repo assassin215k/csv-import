7689ac8b71ead69903a24dae0ca450f3312fa51a
Ihor Fedan <i.fedan@itransition.com>
2021-12-24 12:52:05 +0200

    wip service test

--- tests/Service/ImporterServiceTest.php
+++ tests/Service/ImporterServiceTest.php
@@ -9,21 +9,18 @@
 namespace App\Tests\Service;
 
 use App\Entity\Product;
-use App\Misc\CsvRow;
 use App\Repository\ProductRepository;
 use App\Service\CsvReaderService;
 use App\Service\ImporterService;
 use App\Service\ValidatorService;
 use Doctrine\ORM\EntityManagerInterface;
-use League\Csv\TabularDataReader;
 use PHPUnit\Framework\TestCase;
 
 class ImporterServiceTest extends TestCase {
 	
-	private static CsvReaderService $reader;
-	private static ValidatorService $validator;
+	private static ?CsvReaderService $reader;
+	private static ?ValidatorService $validator;
 	private EntityManagerInterface $manager;
-	private ProductRepository $repository;
 	private ImporterService $service;
 	
 	public static function setUpBeforeClass(): void {
@@ -31,6 +28,13 @@ class ImporterServiceTest extends TestCase {
 		
 		static::$reader    = new CsvReaderService();
 		static::$validator = new ValidatorService();
+	}
+	
+	public static function tearDownAfterClass(): void {
+		parent::tearDownAfterClass();
+		
+		static::$reader    = null;
+		static::$validator = null;
 	}
 	
 	public function setUp(): void {
@@ -73,10 +77,8 @@ class ImporterServiceTest extends TestCase {
 	}
 	
 	public function tearDown(): void {
-		unset( $this->reader );
-		unset( $this->validator );
 		unset( $this->manager );
-		unset( $this->repository );
+		unset( $this->service );
 	}
 	
 	public function testImportWrongPath() {
