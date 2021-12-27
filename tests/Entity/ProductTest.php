02f53b7d6eddf77458f13a303b07ec6df5462a97
Ihor Fedan <i.fedan@itransition.com>
2021-12-27 17:24:09 +0200

    added fixes for phpcs gig hooks

---
+++ tests/Entity/ProductTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Ihor Fedan
+ * Date: 27.12.21
+ * Time: 13:37
+ */
+
+namespace App\Tests\Entity;
+
+use App\Entity\Product;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ProductTest
+ */
+class ProductTest extends TestCase
+{
+    private Product $product;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->product = new Product();
+    }
+
+    /**
+     * @return void
+     */
+    public function testValidPrice():void
+    {
+        $this->product->setCost(15);
+        $this->product->setStock(11);
+
+        $this->assertNotTrue($this->product->isInvalid());
+    }
+
+    /**
+     * @return void
+     */
+    public function testInValidPrice():void
+    {
+        $this->product->setCost(3.5);
+        $this->product->setStock(9);
+
+        $this->assertTrue($this->product->isInvalid());
+    }
+}
