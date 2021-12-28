882104f38fb51f4d678daedacaff570e1c0c83f3
Ihor Fedan <i.fedan@itransition.com>
2021-12-28 13:15:21 +0200

    added custom validator tests

---
+++ tests/Repository/ProductRepositoryTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Ihor Fedan
+ * Date: 28.12.21
+ * Time: 12:59
+ */
+
+namespace App\Tests\Repository;
+
+use App\Repository\ProductRepository;
+use PHPUnit\Framework\TestCase;
+
+class ProductRepositoryTest extends TestCase
+{
+
+    public function testRemoveWithFilterById()
+    {
+
+    }
+}
